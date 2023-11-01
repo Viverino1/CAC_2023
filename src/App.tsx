627f52da86d8a9efb47bffb1ea380ea8ae2a0abd
Viverino1 <Vivek.maddineni@gmail.com>
2023-10-31 23:26:15 -0500

    BrowserRouter Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { Routes, Route } from "react-router-dom"
+import Sidebar from "./components/Sidebar"
+import DashboardPage from "./pages/Dashboard/DashboardPage"
+import FinancePage from "./pages/Finance/FinancePage"
+import TeamPage from "./pages/Team/TeamPage"
+
 export default function App(){
   return(
     <div className="flex h-screen">
-      <div className="bg-red-500 w-20">NAVBAR</div>
-      <div className="w-full bg-background">CONTENT</div>
-      <div className=""></div>
+      <Sidebar/>
+      <div className="w-full bg-background">
+        <Routes>
+          <Route path="/dashboard" element={<DashboardPage/>}/>
+          <Route path="/finance" element={<FinancePage/>}/>
+          <Route path="/growth" element={<DashboardPage/>}/>
+          <Route path="/team" element={<TeamPage/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,29 @@ export default {
   theme: {
     extend: {
       colors: {
-        text: {
-          DEFAULT: "#000000"
-        },
-
-        background: {
-          DEFAULT: "#ffffff"
-        }
-      }
+        'text': '#250604',
+        'background': '#faf5f5',
+        'tertiary': '#F2EDED',
+        'primary': '#af5550',
+        'secondary': '#ecd6d5',
+        'accent': '#da241b',
+       }          
     },
   },
   plugins: [
     ({ addUtilities }) => {
       addUtilities({
-        ".card": {
-          "@apply ": {}
+        ".round": {
+          "@apply rounded-xl border-2 border-secondary": {}
         },
+
+        ".card": {
+          "@apply bg-tertiary round p-4": {}
+        },
+
+        // ".card-primary": {
+        //   "@apply bg-tertiary round p-4": {}
+        // },
       })
     }
   ],
