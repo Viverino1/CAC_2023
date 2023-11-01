622c284a70148a6e145b6f39e6eb7f7ae549b3e5
Viverino1 <Vivek.maddineni@gmail.com>
2023-11-01 17:11:26 -0500

    ye

--- src/pages/Team/TeamPage.tsx
+++ src/pages/Team/TeamPage.tsx
@@ -1,112 +1,173 @@
-import { useState } from "react";
 import { PlusLg } from "react-bootstrap-icons"
+import Task from "../../components/Task"
+import { getInitials } from "../../utils/helpers"
+import { getTeamMembers } from "../../utils/Firebase/firebase"
+
+const teamMembers: TeamMember[] = [
+  {
+    fName: "Vedanth",
+    lName: "Chimakurthy",
+    email: "example@outlook.com",
+  },
+  {
+    fName: "John",
+    lName: "Doe",
+    email: "email@gmail.com",
+  }
+]
+
+const tasks: Task[] = [
+  {
+    startTime: 1698854411000,
+    endTime: 1698894411000,
+    status: "Unstarted",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet arcu maximus, lobortis lacus sed, dapibus quam.",
+    title: "Example Task",
+    highPriority: true,
+    assignedTo: [
+      {
+        fName: "John",
+        lName: "Doe",
+        email: "email@gmail.com",
+      }
+    ],
+    completionDate: 1698895411000
+  },
+  {
+    startTime: 1698864411000,
+    endTime: 1698895411000,
+    status: "In Progress",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet arcu maximus, lobortis lacus sed, dapibus quam.",
+    title: "Finish the Robot Side Panels",
+    highPriority: true,
+    assignedTo: [
+      {
+        fName: "Vedanth",
+        lName: "Chimakurthy",
+        email: "example@outlook.com",
+      },
+      {
+        fName: "John",
+        lName: "Doe",
+        email: "email@gmail.com",
+      }
+    ],
+    completionDate: 1698895411000
+  },
+
+  {
+    startTime: 1698864411000,
+    endTime: 1698895411000,
+    status: "Completed",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet arcu maximus, lobortis lacus sed, dapibus quam.",
+    title: "Finish the Robot Side Panels",
+    highPriority: true,
+    assignedTo: [
+      {
+        fName: "Vedanth",
+        lName: "Chimakurthy",
+        email: "example@outlook.com",
+      },
+      {
+        fName: "John",
+        lName: "Doe",
+        email: "email@gmail.com",
+      }
+    ],
+    completionDate: 1698895411000
+  },
+]
 
 export default function TeamPage(){
-  const status: Status = "Unstarted";
-  const task: Task = {
-    Start: 1234,
-    End: 123455,
-    Title: "Program",
-    Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sapien ipsum, varius eget sollicitudin molestie, vehicula nec magna. Curabitur nec quam risus. Integer tincidunt elit in tortor aliquam, id egestas nulla dapibus. Donec urna massa, aliquet non malesuada quis, laoreet vitae nunc. Morbi eu ultrices nunc, ut cursus nibh. Morbi vel velit lacus. Donec eget ex porta, aliquet massa eget, ultricies eros. Integer vestibulum, massa placerat dignissim egestas, sem ex mollis purus, nec varius purus dui ac velit. Donec id nisl ligula. Pellentesque id lectus sed ante gravida eleifend id sit amet tellus. Vivamus id orci sollicitudin, consequat sapien ut, porta arcu. Mauris quis placerat tortor.",
-  }
-  const [popUp, setPopUp] = useState(false);
-
   return(
-    <div className="flex bg-background w-full h-screen overflow-auto">
-      <div className="w-fit h-full space-y-4 px-2 card overflow-scroll ml-8">
-        <div className="text-text flex">Members</div>
-        <div className="hover:border-2 hover:border-red-500">
-          <img src="testPFP.jpg" className="w-16 aspect-square rounded-full"/>
+    <div className="w-full h-full flex flex-col">
+      <h1 className="text-5xl mt-4 font-bold text-primary w-full text-center">Team Management</h1>
+      <div className="w-full h-full p-4 flex space-x-4">
+        <div className="w-24 h-full card p-2 space-y-4">
+          <h1 className="font-bold text-primary text-center ">Team Members</h1>
+          <div className="center">
+            <div className="peer w-16 font-bold text-3xl text-background center bg-primary aspect-square rounded-full"><PlusLg size={35}/></div>
+          </div>
+          {teamMembers.map((user) => (
+            <div className="center" key={user.email}>
+              <div className="peer w-16 font-bold text-3xl text-background center bg-primary aspect-square rounded-full">{getInitials(user.fName, user.lName)}</div>
+              <Tooltip text={user.fName + " " + user.lName}/>
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div className="flex flex-col lg:flex-row justify-evenly w-full h-screen mx-8 lg:mx-0">
-        <div className="w-full lg:w-1/3 h-full">
-          <div className="title">PRIORITY:</div>
-          <div className="relative card">
-            <h1 className="text-text font-bold text-xs lg:text-3xl">{task.Title}</h1>
-            <p className="lg:text-sm text-[8px] w-48 overflow-ellipsis">{task.Description}</p>
-            <p className="absolute top-0 right-0 p-4 lg:text-sm text-[8px]">
-              <div className="flex items-end flex-col">
-                <p>
-                  START: <input type="date" className="bg-inherit"></input>
-                </p>
-                <p>
-                  END: <input type="date" className="bg-inherit"></input>
-                </p>
-              </div>
-            </p>
-            <p className="flex pt-4 text-sm">
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full"/>
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full -translate-x-2 lg:-translate-x-3"/>
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full -translate-x-4 lg:-translate-x-6"/>
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full -translate-x-6 lg:-translate-x-9"/>
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full -translate-x-8 lg:-translate-x-12"/>
-            </p>
-            <p className="absolute bottom-0 right-0 p-4 text-[10px] lg:text-sm">{status}</p>
-          </div>
-
-          <div className="mt-8 title">LATE:</div>
-          <div className="relative card">
-            <h1 className="text-text font-bold text-xs lg:text-3xl">{task.Title}</h1>
-            <p className="lg:text-sm text-[8px]">{task.Description}</p>
-            <p className="absolute top-0 right-0 p-4 lg:text-sm text-[8px]">
-              <div className="flex items-end flex-col">
-                <p>
-                  START: <input type="date" className="bg-inherit"></input>
-                </p>
-                <p>
-                  END: <input type="date" className="bg-inherit"></input>
-                </p>
-              </div>
-            </p>
-            <p className="flex pt-4 text-sm">
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full"/>
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full -translate-x-2 lg:-translate-x-3"/>
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full -translate-x-4 lg:-translate-x-6"/>
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full -translate-x-6 lg:-translate-x-9"/>
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full -translate-x-8 lg:-translate-x-12"/>
-            </p>
-            <p className="absolute bottom-0 right-0 p-4 text-[10px] lg:text-sm">{status}</p>
-          </div>
+          
+        <div className="w-full h-full overflow-auto space-y-4">
+          <Priority/>
+          <Overdue/>
+          <Upcoming/>
+          <Completed/>
+          <div className="h-12"></div>
         </div>
-
-        <div className="w-full lg:w-1/3 h-full">
-          <div className="title">UPCOMING:</div>
-          <div className="relative card">
-            <h1 className="text-text font-bold text-xs lg:text-3xl">{task.Title}</h1>
-            <p className="lg:text-sm text-[8px]">{task.Description}</p>
-            <p className="absolute top-0 right-0 p-4 lg:text-sm text-[8px]">
-              <div className="flex items-end flex-col">
-                <p>
-                  START: <input type="date" className="bg-inherit"></input>
-                </p>
-                <p>
-                  END: <input type="date" className="bg-inherit"></input>
-                </p>
-              </div>
-            </p>
-            <p className="flex pt-4 text-sm">
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full"/>
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full -translate-x-2 lg:-translate-x-3"/>
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full -translate-x-4 lg:-translate-x-6"/>
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full -translate-x-6 lg:-translate-x-9"/>
-              <img src="testPFP.jpg" className="w-4 md:w-6 lg:w-8 border-2 border-primary aspect-square rounded-full -translate-x-8 lg:-translate-x-12"/>
-            </p>
-            <p className="absolute bottom-0 right-0 p-4 text-[10px] lg:text-sm">{status}</p>
-          </div>
       </div>
     </div>
+  )
+}
 
+function Priority(){
+  return(
+    <div className="w-full space-y-4">
       <div>
-        <PlusLg size={60} className="text-white button-primary rounded absolute bottom-0 right-0 m-5"/>
+        <h1 className="text-2xl font-bold">Priority</h1>
+        <div className="w-full h-0.5 bg-gradient-to-r from-accent"/>
       </div>
-
-      <div className="absolute flex justify-center items-center w-full h-full bg-black text-white">
-        <div className="w-1/2 h-1/3 bg-white">
-          <div className="card-secondary">TITLE</div>
-        </div>
-      </div>
+      {tasks.map((task, i) => (
+        <Task key={i} task={task}/>
+      ))}
     </div>
+  )
+}
+
+function Overdue(){
+  return(
+    <div className="w-full space-y-4">
+      <div>
+        <h1 className="text-2xl font-bold">Overdue</h1>
+        <div className="w-full h-0.5 bg-gradient-to-r from-accent"/>
+      </div>
+      {tasks.map((task, i) => (
+        <Task key={i} task={task}/>
+      ))}
+    </div>
+  )
+}
+
+function Upcoming(){
+  return(
+    <div className="w-full space-y-4">
+      <div>
+        <h1 className="text-2xl font-bold">Upcoming</h1>
+        <div className="w-full h-0.5 bg-gradient-to-r from-accent"/>
+      </div>
+      {tasks.map((task, i) => (
+        <Task key={i} task={task}/>
+      ))}
+    </div>
+  )
+}
+
+function Completed(){
+  return(
+    <div className="w-full space-y-4">
+      <div>
+        <h1 className="text-2xl font-bold">Completed</h1>
+        <div className="w-full h-0.5 bg-gradient-to-r from-accent"/>
+      </div>
+      {tasks.map((task, i) => (
+        <Task key={i} task={task}/>
+      ))}
+    </div>
+  )
+}
+
+function Tooltip(props: {text: string}){
+  return(
+    <span className="z-30 translate-x-2 absolute bg-tertiary border-2 border-secondary peer-hover:opacity-100 opacity-0 transition px-2 py-1 left-24 round scale-0 peer-hover:scale-100 whitespace-nowrap">
+      {props.text}
+    </span>
   )
 }
