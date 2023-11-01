86b9e9d6c444e56d64022a6d2d59bc37c238252b
Verge Robotics <VergeRoboticsFTC@gmail.com>
2023-11-01 04:09:17 -0500

    errors

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import DashboardPage from "./pages/Dashboard/DashboardPage"
 import FinancePage from "./pages/Finance/FinancePage"
 import TeamPage from "./pages/Team/TeamPage"
 import GrowthPage from "./pages/Growth/GrowthPage"
+import AuthPage from "./pages/Auth/AuthPage"
 export default function App(){
   return(
     <div className="flex h-screen w-full fixed">
       <Sidebar/>
-      <div className="w-full bg-background">
+      <div className="w-full h-screen bg-background">
         <Routes>
           <Route path="/dashboard" element={<DashboardPage/>}/>
           <Route path="/finance" element={<FinancePage/>}/>
